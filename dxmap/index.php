104c4d51274b3a1af04d79c99d091a0f3666196f
'Vladimir <'qfminbox@gmail.com'>
2016-02-07 14:33:46 +0300

    brunch-1-initial-commit

--- dxmap/index.php
+++ dxmap/index.php
@@ -1,5 +1,8 @@
 <?php
-
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+require_once (dirname(__FILE__) . '/lib/Telnet.class');
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
